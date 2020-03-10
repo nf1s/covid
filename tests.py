83a4ef2fae1c0dc513d0ebfc6015208a5e5b90f7
ahmednafies <ahmed.nafies@gmail.com>
2020-03-10 16:01:02 +0100

    adds tests

--- tests.py
+++ tests.py
@@ -1,3 +1,19 @@
 # -*- coding: utf-8 -*-
 """ tests module
 """
+
+import covid
+
+
+def test_data():
+    data = covid.data()
+    assert data is not None
+    assert type(data) == list
+    element = data[0]
+    assert "country" in element
+    assert "confirmed" in element
+    assert "deaths" in element
+    assert "recovered" in element
+    assert "latitude" in element
+    assert "longitude" in element
+    assert "last_update" in element
