a05fcfd9d524085b78d1acae473d07029f2e33ba
ahmednafies <ahmed.nafies@gmail.com>
2020-03-10 15:15:58 +0100

    fixes example

---
+++ example.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""Sample example used for testing purposes
+
+"""
+
+from covid import Covid
+
+
+if __name__ == "__main__":
+    Covid().data
--- main.py
+++
@@ -1,4 +0,0 @@
-# -*- coding: utf-8 -*-
-"""Sample main.py used for testing purposes
-
-"""
