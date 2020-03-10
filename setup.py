f9648e779e87c9ebfac69db93244df9505c5de67
ahmednafies <ahmed.nafies@gmail.com>
2020-03-10 15:12:28 +0100

    new release

--- setup.py
+++ setup.py
@@ -1,9 +1,11 @@
+# -*- coding: utf-8 -*-
+
 from setuptools import setup
 
 with open("README.md") as f:
     long_description = f.read()
 
-VERSION = "0.0.1"
+VERSION = "0.0.2"
 
 setup(
     name="covid",
