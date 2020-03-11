21752db1a5634c6ea65c902817bb4ede30f3a068
ahmednafies <ahmed.nafies@gmail.com>
2020-03-11 13:56:09 +0100

    fixes example and README

--- example.py
+++ example.py
@@ -7,18 +7,16 @@ from covid import Covid
 
 if __name__ == "__main__":
     covid = Covid()
+    print(covid.get_data())
 
     deaths = covid.get_total_deaths()
     confirmed = covid.get_total_confirmed_cases()
     recovered = covid.get_total_recovered()
 
-    countries = covid.list_countries()
-    sweden_cases = covid.get_status_by_country("sweden")
-
-    print(covid.get_data())
-
     print(f"deaths: {deaths}, confirmed: {confirmed}, recovered: {recovered}")
 
-    print(f"sweden_cases: {sweden_cases}")
-
+    countries = covid.list_countries()
     print(f"countries: {countries}")
+
+    sweden_cases = covid.get_status_by_country("sweden")
+    print(f"sweden_cases: {sweden_cases}")
