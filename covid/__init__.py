a69add2d25afd8eb6e60a5a18fd501b4700f1706
Ahmed <ahmed@Ahmeds-MacBook-Pro.local>
2020-03-24 21:36:45 +0100

    adds headers

--- covid/__init__.py
+++ covid/__init__.py
@@ -2,6 +2,15 @@
 from .john_hopkins import Covid as JohnHopkinsCovid
 from .worldometers import Covid as WorldometersCovid
 
+__author__ = "Ahmed Nafies Okasha Mohamed"
+__copyright__ = "Copyright 2020, Ahmed Nafies Okasha Mohamed"
+
+__license__ = "MIT"
+__version__ = "2.0.0"
+__maintainer__ = "Ahmed Nafies"
+__email__ = "ahmed.nafies@gmail.com"
+__status__ = "Production"
+
 
 def Covid(source="john_hopkins"):
     if source == "worldometers":
