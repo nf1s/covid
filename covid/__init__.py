baeeddaba1bba0b68a5ba34e81e6abe0dc913d68
Ahmed <ahmed@Ahmeds-MacBook-Pro.local>
2020-03-24 18:30:07 +0100

    adds functionality for worldometers

--- covid/__init__.py
+++ covid/__init__.py
@@ -1,3 +1,9 @@
 # -*- coding: utf-8 -*-
-from .john_hopkins import Covid as JohnHopkinsUniversityCovid
+from .john_hopkins import Covid as JohnHopkinsCovid
 from .worldometers import Covid as WorldometersCovid
+
+
+def Covid(source="john_hopkins"):
+    if source == "worldometers":
+        return WorldometersCovid()
+    return JohnHopkinsCovid()
--- covid/worldometers/covid.py
+++ covid/worldometers/covid.py
@@ -16,21 +16,24 @@ class Covid:
         self.__fetch()
         self.__set_data()
 
-    def __fetch(self):
+    def _fetch(self):
         response = requests.get(self._url)
         soup = BeautifulSoup(response.text, "html.parser")
         table = soup.find("table", attrs={"class": "main_table_countries"})
         headers = table.find_all("th")
-        self._headers = self.__format_headers(headers)
+        self._headers = self._format_headers(headers)
         self._rows = table.tbody.find_all("tr")
+        self._total_cases = soup.find_all(
+            "div", attrs={"class": "maincounter-number"}
+        )
 
-    def __format_headers(self, headers):
+    def _format_headers(self, headers):
         headers = [underscore(header.text) for header in headers]
         headers[0] = headers[0].split(",").pop(0)
         headers[8] = headers[8].replace("\xa0", "")
         return headers
 
-    def __set_data(self):
+    def _set_data(self):
 
         countries = (
             [attr.text.strip() for attr in row if attr != "\n"]
@@ -46,3 +49,25 @@ class Covid:
 
     def get_status_by_country_name(self, name):
         return dict(zip(self._headers, self._data[name.lower()]))
+
+    def list_countries(self):
+        return self._data.keys()
+
+    @staticmethod
+    def _to_num(string):
+        return int(string.strip().replace(",", ""))
+
+    def get_total_confirmed_cases(self):
+        return self._to_num(self._total_cases[0].span.text)
+
+    def get_total_deaths(self):
+        return self._to_num(self._total_cases[1].span.text)
+
+    def get_total_recovered(self):
+        return self._to_num(self._total_cases[2].span.text)
+
+    def get_total_active_cases(self):
+        confirmed = self.get_total_confirmed_cases()
+        deaths = self.get_total_deaths()
+        recovered = self.get_total_recovered()
+        return confirmed - (recovered + deaths)
