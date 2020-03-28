29c07ee280c4ff9e0c1f95bbebc47ab9458ab602
Ahmed <ahmed.nafies@gmail.com>
2020-03-28 11:35:41 +0100

    adds source and docstring

--- covid/worldometers/covid.py
+++ covid/worldometers/covid.py
@@ -4,9 +4,12 @@
 """
 import requests
 from bs4 import BeautifulSoup
-from .models import CovidModel
+from covid.worldometers.models import CovidModel
+from covid import config
 
 URL = "https://www.worldometers.info/coronavirus/"
+
+SOURCE = config.WORLDOMETERS
 
 
 class Covid:
@@ -15,8 +18,13 @@ class Covid:
         self._data = {}
         self._fetch()
         self._set_data()
+        self.source = SOURCE
 
     def _fetch(self):
+        """Method get all data when the class is inistantiated
+            1. parses html
+            2. gets all country data
+        """
         response = requests.get(self._url)
         soup = BeautifulSoup(response.text, "html.parser")
         table = soup.find("table", attrs={"class": "main_table_countries"})
@@ -28,6 +36,9 @@ class Covid:
         )
 
     def _set_data(self):
+        """Method formats data to make it easily callable by country name
+        """
+
         countries = (
             [attr.text.strip() for attr in row if attr != "\n"]
             for row in self._rows
@@ -35,17 +46,43 @@ class Covid:
         self._data = {country[0].lower(): country for country in countries}
 
     def _format(self, _list: list) -> list:
+        """Method formats a list and returns a fomatted one
+        1. removes ','
+        2. if there is no value it adds 0
+        
+        Args:
+            _list (list): input list to be formatted
+        
+        Returns:
+            list: output formatted list
+        """
         _list = [val.strip().replace(",", "") for val in _list]
         return [val if val else 0 for val in _list]
 
     def get_data(self) -> list:
+        """Method returns a list of all of the data from worldometers after being formatted
+        
+        Returns:
+            list: List of country data
+        """
 
         return [
             CovidModel(**dict(zip(self._headers, self._format(val)))).dict()
             for val in self._data.values()
         ]
 
-    def get_status_by_country_name(self, country_name) -> dict:
+    def get_status_by_country_name(self, country_name: str) -> dict:
+        """Method gets country status
+        
+        Args:
+            country_name (str): country name e.g "Sweden"
+        
+        Raises:
+            ValueError: when country name is not correct
+        
+        Returns:
+            dict: Country information
+        """
         try:
             country_data = dict(
                 zip(
@@ -63,19 +100,48 @@ class Covid:
         return list(self._data.keys())
 
     @staticmethod
-    def _to_num(string) -> int:
+    def _to_num(string: str) -> int:
+        """formats string numbers and converts them to an integer
+        e.g '123,456' -> 123456
+        
+        Args:
+            string (str): input string number
+        
+        Returns:
+            int: output integer number
+        """
         return int(string.strip().replace(",", ""))
 
     def get_total_confirmed_cases(self) -> int:
+        """Method gets the total number of confirmed cases
+        
+        Returns:
+            int: Number of confirmed cases
+        """
         return self._to_num(self._total_cases[0].span.text)
 
     def get_total_deaths(self) -> int:
+        """Method gets the total number of deaths
+        
+        Returns:
+            int: Total number of deaths
+        """
         return self._to_num(self._total_cases[1].span.text)
 
     def get_total_recovered(self) -> int:
+        """Method gets the total number of recovered cases
+        
+        Returns:
+            int: Total number of recovered cases
+        """
         return self._to_num(self._total_cases[2].span.text)
 
     def get_total_active_cases(self) -> int:
+        """Method gets the total number of active cases
+        
+        Returns:
+            int: Total number of active cases
+        """
         confirmed = self.get_total_confirmed_cases()
         deaths = self.get_total_deaths()
         recovered = self.get_total_recovered()
