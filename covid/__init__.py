f8ee6226d7844ea030206939586f31a1bd567af4
Ahmed Nafies <ahmed.nafies@gmail.com>
2021-05-30 10:06:52 +0200

    updates dependencies

--- covid/__init__.py
+++ covid/__init__.py
@@ -1,12 +1,12 @@
 # -*- coding: utf-8 -*-
+from covid import config
 from covid.john_hopkins import Covid as JohnHopkinsCovid
 from covid.worldometers import Covid as WorldometersCovid
-from covid import config
 
 __author__ = "Ahmed Nafies Okasha Mohamed <ahmed.nafies@gmail.com>"
 __copyright__ = "Copyright 2020, Ahmed Nafies Okasha Mohamed"
 __license__ = "MIT"
-__version__ = "2.4.1"
+__version__ = "2.4.2"
 
 
 def Covid(source=config.JOHN_HOPKINS):
